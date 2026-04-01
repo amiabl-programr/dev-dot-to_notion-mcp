265e5eb8f9d22dbd6a7891694ae6bab05dc85775
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-04-01 19:35:46 +0100

    feat(auth): remove express-session middleware to simplify authentication flow

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import session from 'express-session';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -15,19 +14,6 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'ngrok-skip-browser-warning'],
   });
-
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET!,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 10 * 60 * 1000,
-      },
-    }),
-  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
