934060d1b2d1fa4060098ea7c3b725e3785d887f
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-04-01 19:35:58 +0100

    feat(prisma): refactor PrismaService to initialize Pool with SSL configuration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaPg({
+    const pool = new Pool({
       connectionString: process.env.DATABASE_URL as string,
+      ssl: {
+        rejectUnauthorized: true,
+      },
     });
+
+    const adapter = new PrismaPg(pool);
     super({ adapter });
   }
 }
